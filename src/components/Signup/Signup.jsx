d68c86f61e5dbd183fa7551f5d899ae0224b9fd2
Sreerag Mohan <sreeraagmohancv@gmail.com>
2024-01-24 19:11:56 +0530

    redux installed and created

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -4,27 +4,28 @@ import { Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
-
-    const inputClass = "rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2  ring-gray-900 outline-0"
+  const inputClass =
+    "rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2  ring-gray-900 outline-0";
+  const labelClass =
+    "block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2";
+  const invalidClass =
+    "block text-gray-600 cursor-text text-sm leading-[140%] font-normal text-[#e82d2d] mt-1";
 
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <Card
         sx={{ maxWidth: 345, justifyContent: "center", marginTop: "10rem" }}
       >
-        <div class="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
-          <div class="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">
-            Welcome to <span class="text-[#7747ff]">UPOUQU</span>
+        <div className="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
+          <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">
+            Welcome to <span className="text-[#7747ff]">UPOUQU</span>
           </div>
-          <div class="text-sm font-normal mb-4 text-center text-[#1e0e4b]">
+          <div className="text-sm font-normal mb-4 text-center text-[#1e0e4b]">
             Create your account
           </div>
-          <form class="flex flex-col gap-3">
-            <div class="block relative">
-              <label
-                for="username"
-                class="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-              >
+          <form className="flex flex-col gap-3">
+            <div className="block relative">
+              <label for="username" className={labelClass}>
                 Username
               </label>
               <input
@@ -33,12 +34,10 @@ export default function Signup() {
                 name="username"
                 className={inputClass}
               />
+              {/* <p className={invalidClass}>invalid username</p> */}
             </div>
             <div class="block relative">
-              <label
-                for="email"
-                class="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-              >
+              <label for="email" className={labelClass}>
                 Email
               </label>
               <input
@@ -47,36 +46,35 @@ export default function Signup() {
                 name="email"
                 className={inputClass}
               />
+              {/* <p className={invalidClass}>invalid username</p> */}
             </div>
             <div class="block relative">
-              <label
-                for="password"
-                class="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2"
-              >
+              <label for="password" className={labelClass}>
                 Password
               </label>
               <input
                 type="text"
                 id="password"
                 name="password"
-                class="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
+                className={inputClass}
               />
+              {/* <p className={invalidClass}>invalid username</p> */}
             </div>
             <div>
-              <a class="text-sm text-[#7747ff]" href="#">
+              <a className="text-sm text-[#7747ff]" href="#">
                 Forgot your password?
               </a>
             </div>
             <button
               type="submit"
-              class="bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal"
+              className="bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal"
             >
               Submit
             </button>
           </form>
-          <div class="text-sm text-center mt-[1.6rem]">
+          <div className="text-sm text-center mt-[1.6rem]">
             Already have an account?
-            <Link to="/login" class="text-sm text-[#7747ff]" href="#">
+            <Link to="/login" className="text-sm text-[#7747ff]" href="#">
               Login here!
             </Link>
           </div>
---
+++ src/store/AuthSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: {
+    isLoggedIn: false,
+    isVerified: false,
+    userId: null,
+  },
+  reducers: {
+    handleLogin(state, action) {
+      state.isLoggedIn = true;
+      state.isVerified = true;
+      state.userId = action.payload.id;
+      localStorage.setItem("token", action.payload.token);
+    },
+  },
+});
+
+export const { handleLogin } = authSlice.actions;
+export default authSlice;

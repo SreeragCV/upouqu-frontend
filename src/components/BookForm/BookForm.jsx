ac87658ce6b17306f8777ad467efab022d154c3a
Sreerag Mohan <sreeraagmohancv@gmail.com>
2024-02-06 23:57:57 +0530

    frontend file validation logic added and validated

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Errors from "../../pages/Error";
-import { isNotEmpty, isNumber } from "../../utils/validations";
+import { isNumber } from "../../utils/validations";
 
 function BookForm() {
   const [name, setName] = useState("");
@@ -11,6 +11,7 @@ function BookForm() {
   const [image, setImage] = useState("");
   const [bookPdf, setBookPdf] = useState("");
   const [serverError, setServerError] = useState("");
+  const [fileError, setFileError] = useState();
   const [didEdit, setDidEdit] = useState({
     name: false,
     genre: false,
@@ -33,6 +34,22 @@ function BookForm() {
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
+
+      let fileErrors = {};
+
+      if (!image || !image.type.startsWith("image/")) {
+        console.log("hello");
+        fileErrors.image = "Choose a proper image ( .jpg, .png, .jpeg etc...)";
+      }
+
+      if (!bookPdf || bookPdf.type !== "application/pdf") {
+        console.log("pdf");
+        fileErrors.pdf = "Choose a proper pdf (eg- .pdf)";
+      }
+
+      if (Object.keys(fileErrors).length > 0) {
+        return setFileError(fileErrors);
+      }
 
       const formData = new FormData();
       formData.append("book_name", name);
@@ -73,6 +90,8 @@ function BookForm() {
     return <Errors />;
   }
 
+  console.log(fileError);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen dark mt-6 mb-0">
       <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-6">
@@ -99,6 +118,7 @@ function BookForm() {
           {nameIsInvalid ? (
             <p className="mt-1 text-sm text-red-500">Enter a valid book name</p>
           ) : null}
+
           <select
             name="genre"
             placeholder="genre"
@@ -124,6 +144,7 @@ function BookForm() {
           {genreIsInvalid ? (
             <p className="mt-1 text-sm text-red-500">Enter a valid genre</p>
           ) : null}
+
           <input
             name="price"
             placeholder="Price"
@@ -143,6 +164,7 @@ function BookForm() {
           {priceIsInvalid ? (
             <p className="mt-1 text-sm text-red-500">Price must be a number</p>
           ) : null}
+
           <textarea
             placeholder="Description"
             className="bg-gray-700 text-gray-200 border-0 rounded-md p-3 mt-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
@@ -159,8 +181,11 @@ function BookForm() {
             onBlur={() => handleBlur("description")}
           ></textarea>
           {descriptionIsInvalid ? (
-            <p className="mt-1 text-sm text-red-500">Enter a valid description</p>
+            <p className="mt-1 text-sm text-red-500">
+              Enter a valid description
+            </p>
           ) : null}
+
           <label className="mt-4" htmlFor="book">
             Book (pdf)
           </label>
@@ -172,6 +197,10 @@ function BookForm() {
             type="file"
             onChange={(e) => setBookPdf(e.target.files[0])}
           />
+          {fileError && fileError.pdf ? (
+            <p className="mt-1 text-sm text-red-500">{fileError.pdf}</p>
+          ) : null}
+
           <label className="mt-4" htmlFor="image">
             Cover Image
           </label>
@@ -183,6 +212,10 @@ function BookForm() {
             type="file"
             onChange={(e) => setImage(e.target.files[0])}
           />
+          {fileError && fileError.image ? (
+            <p className="mt-1 text-sm text-red-500">{fileError.image}</p>
+          ) : null}
+
           <button
             className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-6 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
             type="submit"
